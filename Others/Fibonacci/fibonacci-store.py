cec1f764573209d24c0bce19b54c0e32d5e47749
tzicatl <nnieto@noenieto.com>
2012-09-01 14:58:02 -0500

    Improved fibonacci and new spin of fibonacci algorithm

--- Others/Fibonacci/fibonacci-store.py
+++ Others/Fibonacci/fibonacci-store.py
@@ -2,36 +2,28 @@
 """
 This is the fibonacci algorithm as narrated in the book Algorithms,
 Copyright c 2006 S. Dasgupta, C. H. Papadimitriou, and U. V. Vazirani, 
-Chapter 0, Page 12
+Chapter 0, Page 13
 
-This algorithm computes N fibonacci numbers with a recursive function.
-
-For N>30 the fib sequence will be start to take longer and longer.
+This algorithm computes N fibonacci numbers, but instead of using recursive
+function, it just stores past results in an array, so we avoid expensive
+calculations for past values.
 
 In a core i5 with 4 GB of RAM this takes:
 
-time ./fibonacci.py 100
-Generating 30 fibonacci numbers:
-0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229
+time ./fibonacci-store.py 100
+Generating 300 fibonacci numbers:
+real    0m0.048s
+user    0m0.038s
+sys 0m0.009s
 
-real    0m1.054s
-user    0m1.036s
-sys 0m0.011s
 
 
 """
 import sys
+results = [0,1]
 
 def fib(n):
-    """
-    Oh! recursivity!
-    """
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fib(n-1) + fib (n-2)
-
+    results[n] = results[n-1] + results[n-2]
 
 if __name__ == '__main__':
     #This is just for taking arguments in the command line.
@@ -52,5 +44,8 @@ if __name__ == '__main__':
         
     #Finally
     print "Generating %i fibonacci numbers:" % N
-    for i in range(N):
-        print fib(i),
+    #Reserve memory for all results
+    results = range(N)
+    for i in range(2, N):
+        fib(i)
+    # print results
